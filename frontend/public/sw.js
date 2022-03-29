b6d3565047a875fde6298d6da2ddfdd13f4d3808
aryan.tripathi <aryan.tripathi@netcorecloud.com>
2022-03-29 12:12:10 +0530

    update service worker

---
+++ frontend/public/sw.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBaRH9FrP45fF2xw6voIOQrOnB3Zr3Kx2c",
+  messagingSenderId: "842008249706",
+  appId: "1:842008249706:web:6f8df63a02a4efc0c8d113",
+  projectId: "webpush1-6d9ad",
+  user_key: "ADGMOT35CHFLVDHBJNIG50K96BCUNNT01CHH00USCID0MIUCC5RG",
+  siteid: "c6874bf2e38e64dcf89732290fb2b734",
+};
+importScripts("//cdnt.netcoresmartech.com/swv4-dev0.js");
